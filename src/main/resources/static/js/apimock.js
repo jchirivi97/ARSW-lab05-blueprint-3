3e6e88d4469f7c1ef4f0a338568392b1d5e0d979
Daniel Rodriguez <daniel.rodriguez-vi@mail.escuelaing.edu.co>
2020-03-02 23:12:43 -0500

    avance del lab 5

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -90,7 +90,7 @@ var apimock = (function () {
 
     return {
         getBlueprintsByAuthor: function(author, callback) {
-            callback(null, mockdata[author]);
+            callback(mockdata[author]);
         },
 
         getBlueprintsByNameAndAuthor: function(name, author, callback) {
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,30 @@
-var moduleprinc = ( function principal() {
+var moduleprinc = ( function () {
+
+    var funcion = function (planes) {
+       alert(planes.length);
+        if(planes != null) {
+            var objetos = planes.map(function (blueprint) {
+                return {key: blueprint.name, value: blueprint.points.length}
+            });
+            $("table tbody").empty();
+            objetos.map(function () {
+                var columna = '<tr> <td>${blueprint.key}</td><td>${bluprint.value}</td><td><p>boton open </p></td></tr>';
+                $("#table tbody").append(columna);
+                alert(columna);
+             });
+        }
 
 
+    }
 
+    return {
 
+        getblueprintsAuthor : function () {
+            nameauthor = document.getElementById("name").value;
+            apimock.getBlueprintsByAuthor(nameauthor,funcion)
+        }
+    }
 }
+
 
 )();
