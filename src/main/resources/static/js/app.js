75bfdbfcf3d1f6ca0e9c2835ed2a3b1fbcf0f17a
jimmy chirivi <jimmychirivi09@gmail.com>
2020-03-05 00:09:51 -0500

    total puntos y nombre del user

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,7 @@
 var moduleprinc = ( function () {
 
     var funcion = function (planes) {
+        var total = 0;
         if(planes != null) {
             var objetos = planes.map(function (bp) {
                 return {key: bp.name, value: bp.points.length}
@@ -8,8 +9,13 @@ var moduleprinc = ( function () {
             $("table tbody").empty();
             objetos.map(function (bp) {
                 var columna = '<tr><td>'+bp.key+'</td><td>'+bp.value+'</td><td> <button type="button" class="btn btn-primary" onclick="moduleprinc.getblueprintsAuthor()"> Open </button>  </td></tr>';
+                total = total + bp.value;
                 $("table tbody").append(columna);
+                document.getElementById("sumapoints").innerHTML = "Total user points: "+ total;
              });
+
+            document.getElementById("nomauthor").innerHTML = nameauthor + "'s blueprints";
+
         }
 
 
